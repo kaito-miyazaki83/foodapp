dbfad6197ce1546d5bc5a5d57ea3faad15aa8fbd
kaito-miyazaki83 <miyazaki-k@illmatics.co.jp>
2024-11-06 17:14:04 +0900

    ホーム画面の作成

---
+++ src/Views/HomeView.vue
@@ -0,0 +1,79 @@
+<script setup>
+import HeaderComponent from '../components/HeaderComponent.vue'
+import FooterComponent from '../components/FooterComponent.vue'
+
+import { ref } from 'vue'
+
+const count = ref(0)
+</script>
+
+<template>
+    <header>
+    <HeaderComponent></HeaderComponent>
+  </header>
+    <main>
+        <p class="contents">ここがHomeです</p>
+        <button @click="count++">You clicked me {{ count }} times.</button>
+    </main>
+    <footer>
+        <FooterComponent></FooterComponent>
+    </footer>
+    <BaseLayout>
+    <template #header>
+      <HeaderComponent></HeaderComponent>
+    </template>
+
+    <template #default>
+      <p>A paragraph for the main content.</p>
+      <p>And another one.</p>
+    </template>
+
+    <template #footer>
+        <FooterComponent></FooterComponent>
+    </template>
+  </BaseLayout>
+</template>
+
+
+
+
+
+<style scoped>
+body {
+    min-height: calc(100vh - 40px);
+    position: relative;
+    box-sizing: border-box;
+}
+
+.contents {
+    font-size: 30px;
+    margin: 50px 0 0 0;
+}
+
+/* ///////////////////////////////////////// */
+/*　後でApp.vueへ移す設定 */
+/* ///////////////////////////////////////// */
+/* 青色の文字(ログインネームなど) */
+.blue {
+    color: #2296f3;
+}
+
+header {
+    background: #efefef;
+    width: auto;
+}
+
+footer {
+    background: #efefef;
+    width: 100%;
+    position: absolute;
+    bottom: -40px;
+    height: 40px;
+}
+
+body {
+    min-height: calc(100vh - 40px);
+    position: relative;
+    box-sizing: border-box;
+}
+</style>
---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,21 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+    <div class="copyright">©2024 Vue Firebase Example, Ltd. All rights reserved.</div>
+</template>
+
+<style scoped>
+
+footer .copyright {
+    font-size: 11px;
+    margin: 12px auto 0 auto;
+}
+
+</style>
