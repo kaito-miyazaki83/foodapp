96acbded10142ad1d9a4fe5f4de12c37673ccb77
kaito-miyazaki83 <miyazaki-k@illmatics.co.jp>
2024-11-07 14:58:02 +0900

    firebaseset

---
+++ src/firebase_settings/index.js
@@ -0,0 +1,29 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDhCcYW5NEwD5IOJ39Tm6zjsPAVgFq8cbY",
+  authDomain: "foodapp-2291b.firebaseapp.com",
+  databaseURL: "https://foodapp-2291b-default-rtdb.firebaseio.com",
+  projectId: "foodapp-2291b",
+  storageBucket: "foodapp-2291b.firebasestorage.app",
+  messagingSenderId: "355169340440",
+  appId: "1:355169340440:web:4e79966f12134d2eb2476d",
+  measurementId: "G-RMXTMNMM3C"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app);
+// Initialize Authentication
+const auth = getAuth()
+
+export default { analytics, db, auth };
