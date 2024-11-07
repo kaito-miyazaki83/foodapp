3ae83e11ce0ceaefd1a301a955711ca1f25e7660
kaito-miyazaki83 <miyazaki-k@illmatics.co.jp>
2024-11-07 15:57:22 +0900

    login部分作成

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,129 @@
+<template>
+    <div class="header_container">
+        <div class="header_logo">
+            <div class="img_container">
+                <img src="../assets/logo.png" alt="logo">
+            </div>
+            <p>Vue Firebase Example</p>
+        </div>
+        <div>
+            <div class="login_name blue">&nbsp;{{ login_name }}</div>
+            <div class="header_menu">
+                <div class="menu_item" v-on:click="goToHome">Home</div>
+                <div class="menu_item" v-on:click="goToFood">Food</div>
+                <div class="menu_item" v-on:click="confirmLogout">Logout</div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import { signOut, onAuthStateChanged } from 'firebase/auth';
+import { auth } from '../firebase_settings/index.js';
+
+export default {
+  name: 'HeaderComponent',
+  data() {
+    return {
+      login_name: "",
+    };
+  },
+  methods: {
+    confirmLogout() {
+      const result = confirm('ログアウトしますか？');
+      if (!result) { return; }
+      this.logOut();
+    },
+    logOut() {
+      signOut(auth).then(() => {
+        this.$router.push('/login');
+        console.log("ログアウト成功");
+        alert('ログアウトしました');
+      }).catch((error) => {
+        console.log('ログアウトエラー: error ->' + error);
+        alert('ログアウト処理でエラーが発生しました');
+      });
+    },
+    showLoginEmail() {
+      onAuthStateChanged(auth, (user) => {
+        if (user) {
+          const email = user.email;
+          this.login_name = 'ログイン中: ' + email + ' さん';
+        } else {
+          this.login_name = '未ログイン: ゲストユーザーさん';
+        }
+      });
+    },
+    goToHome() {
+      const nowRoute = this.$route.path;
+      if (nowRoute != '/') {
+        this.$router.push('/');
+      } else {
+        window.location.reload();
+      }
+    },
+    goToFood() {
+      const nowRoute = this.$route.path;
+      if (nowRoute != '/food') {
+        this.$router.push('/food');
+      } else {
+        window.location.reload();
+      }
+    },
+  },
+  mounted() {
+    this.showLoginEmail();
+  }
+};
+</script>
+
+<style scoped>
+/* ///////////////////////////////////////// */
+/* ヘッダー関連 */
+/* ///////////////////////////////////////// */
+.header_container {
+    display: flex;
+    justify-content: space-between;
+    padding: 7px 25px 15px 25px;
+    height: 60px;
+    width: auto;
+    background: #efefef;
+}
+.header_logo {
+    display: flex;
+    width: fit-content;
+}
+.header_logo p {
+    font-size: 35px;
+    padding: 0 0 0 10px;
+}
+.img_container {
+    width: 100px;
+}
+img {
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
+}
+
+.login_name {
+    text-align: right;
+}
+.header_menu {
+    display: flex;
+    justify-content: right;
+    padding: 2px 0 5px 0;
+    margin: 0 0 0 10px;
+}
+.menu_item {
+    width: max-content;
+    margin-left: 30px;
+}
+.menu_item:hover {
+    cursor: pointer;
+    color: #2296f3;
+    border-bottom: solid 3px #2296f3;
+}
+
+</style>
+
