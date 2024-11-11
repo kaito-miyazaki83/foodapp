5d518c6ccd7b50e3878ad584fcb0976b487222e2
kaito-miyazaki83 <miyazaki-k@illmatics.co.jp>
2024-11-11 18:44:30 +0900

    エラーが出たため変更

---
+++ src/views/MealView.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+      <h1>Meal View</h1>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'MealView'
+  };
+  </script>
+  
+  <style scoped>
+  /* スタイルを追加 */
+  </style>
+  
---
+++ src/views/RecipeView.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+      <h1>Recipe View</h1>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'RecipeView'
+  };
+  </script>
+  
+  <style scoped>
+  /* スタイルを追加 */
+  </style>
+  
