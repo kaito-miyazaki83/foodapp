8c4bd15b8a918b7b8dee3415dac6dc19fac1f164
kaito-miyazaki83 <miyazaki-k@illmatics.co.jp>
2024-11-13 16:54:17 +0900

    ヘッダー変更

--- src/views/RestaurantView.vue
+++ src/views/RestaurantView.vue
@@ -1,16 +1,44 @@
 <template>
-    <div>
-      <h1>Restaurant View</h1>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    name: 'RestaurantView'
-  };
-  </script>
-  
-  <style scoped>
-  /* スタイルを追加 */
-  </style>
-  
+
+  <body>
+      <HeaderComponent></HeaderComponent>
+
+      <main>
+          <h1>HOT PEPPPER</h1>
+          <HotpeperComponent></HotpeperComponent>
+      </main>
+
+      <FooterComponent></FooterComponent>
+  </body>
+</template>
+
+<script>
+import HeaderComponent from '../components/common/HeaderComponent.vue'
+import FooterComponent from '../components/common/FooterComponent.vue'
+import HotpeperComponent from '../components/restaurants/HotpeperComponent.vue'
+
+export default {
+name: 'HomeView',
+components: {
+  HeaderComponent,
+  FooterComponent,
+  HotpeperComponent
+},
+data() {
+  return {};
+},
+methods: {},
+mounted() {}
+};
+</script>
+
+<style scoped>
+/* ///////////////////////////////////////// */
+/* コンテンツ関連 */
+/* ///////////////////////////////////////// */
+
+.contents {
+  font-size: 30px;
+  margin: 50px 0 0 0;
+}
+</style>
