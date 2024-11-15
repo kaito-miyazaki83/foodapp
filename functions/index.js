e4ab5372089a0ad939223bb4ea4db67a948bcc8b
kaito-miyazaki83 <miyazaki-k@illmatics.co.jp>
2024-11-15 17:12:29 +0900

    検索画面変更

--- functions/index.js
+++ functions/index.js
@@ -1,23 +1,31 @@
 const functions = require('firebase-functions');
 const axios = require('axios');
-const cors = require('cors')({ origin: true });
+const cors = require('cors');
+const express = require('express');
+const app = express();
 
-exports.searchShops = functions.https.onRequest((req, res) => {
-  cors(req, res, () => {
-    const shopName = req.query.shopName;
-    axios.get('http://webservice.recruit.co.jp/hotpepper/gourmet/v1/', {
-      params: {
-        key: 'a99653fd49e99132',
-        keyword: shopName,
-        format: 'json'
-      }
-    })
-    .then(response => {
-      res.json(response.data.results.shop);
-    })
-    .catch(error => {
-      res.status(error.response ? error.response.status : 500).send(error.message);
-    });
+app.use(cors({ origin: true }));
+
+const apiUrl = 'http://webservice.recruit.co.jp/hotpepper/gourmet/v1/';
+const apiKey = 'a99653fd49e99132'; // あなたのAPIキーをここに設定
+
+app.get('/searchShops', (req, res) => {
+  const { keyword } = req.query;
+  axios.get(apiUrl, {
+    params: {
+      key: apiKey,
+      keyword: keyword,
+      format: 'json'
+    }
+  })
+  .then(response => {
+    res.json(response.data.results.shop);
+  })
+  .catch(error => {
+    const status = error.response ? error.response.status : 500;
+    const message = error.response ? error.response.data.message : error.message;
+    res.status(status).send({ message });
   });
 });
 
+exports.api = functions.https.onRequest(app);
--- src/api/searchShops.js
+++ src/api/searchShops.js
@@ -1,20 +1,28 @@
-import axios from 'axios';
+import fs from 'fs';
+import fetch from 'node-fetch';
+const API_KEY = '[APIキーを記載する]';
+//const KEYWORD = 'おじんじょ';
+const KEYWORD = 'らんまん食堂 恵比寿';
 
-const searchShops = (shopName) => {
-  return new Promise((resolve, reject) => {
-    axios.get('https://foodapp-2291b.cloudfunctions.net/searchShops', {
-      params: {
-        shopName: shopName
-      }
-    })
-    .then(response => {
-      resolve(response.data);
-    })
-    .catch(error => {
-      reject(error.message);
-    });
-  });
-};
+// https://webservice.recruit.co.jp/doc/hotpepper/reference.html
+const URL = 'http://webservice.recruit.co.jp/hotpepper/shop/v1/?key=' + API_KEY + '&keyword=' + encodeURIComponent(KEYWORD) + '&format=json';
+// console.log(URL);
 
-export default searchShops;
+async function main() {
+  try {
+    const res = await fetch(URL);
+    if (!res.ok) {
+      throw new Error(`${res.status} ${res.statusText}`);
+    }
+    const data = await res.text();
+    console.log(data)
+    const obj = JSON.parse(data)
+    console.log('店名:' +obj.results.shop[0].name)
+    console.log('住所:' +obj.results.shop[0].address)
+    console.log('URL ' +obj.results.shop[0].urls.pc)
+  } catch (err) {
+    console.error(err);
+  }
+}
 
+main();
